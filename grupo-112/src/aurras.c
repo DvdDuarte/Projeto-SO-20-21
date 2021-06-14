997dec2e4dd9fd084006ccf09613c80025e57e18
sdasl03 <sdaslira@gmail.com>
2021-06-14 20:22:04 +0100

    acho q ja esta mais ou menos

--- grupo-112/src/aurras.c
+++ grupo-112/src/aurras.c
@@ -8,58 +8,26 @@ int main(int argc, char const *argv[]) {
     char buffer[1024];
     int bytesRead = 0;
     
-    if(argc>1){
-	    switch(argv[1]){
-		case "status":
-			//executar status
-			break;
-    		case "transform":
-			//executar os filtros
-			char * fOrig=strdup(argv[2]);
-			char * fFinal=strdup(argv[3]);
-			int i=4;int j=0;
-			char *filtros[argc-i];
-			while(i<=argc){
-				filtros[j]=strdup(argv[i]);
-				j++;i++;
-			}
-			//blabla
-			break;
-		//outros casos
-	    }
-    } else {
+    int cs_fd = open("/tmp/fifo_client_server", O_WRONLY);
     
-    }
-    
-    return 0;	
-   }    
+    buffer[0]='\0';
+    char * fOrig=strdup(argv[2]);
+    strcat(buffer,fOrig);
+    strcat(buffer," ");
+    char * fFinal=strdup(argv[3]);
+    strcat(buffer,fFinal);
+    strcat(buffer," ");
+    int i=4;int j=0;
+    char *filtros[argc-i];
+    while(i<=argc){
+	filtros[j]=strdup(argv[i]);
+        strcat(buffer,filtros[j]);
+        strcat(buffer," ");
 
-		    
-		    
-/*	bulhufas daqui para baixo, mas pode ser util
- *
- *	while((bytesRead= read(0,buffer,1024))>0){
-	int cs_fd = open("/tmp/fifo_client_server", O_WRONLY);
-	int sc_fd = open("/tmp/fifo_server_client", O_RDONLY);
-	write(cs_fd,buffer,bytesRead);
-	close(cs_fd);
-	
-	while((bytesRead = read(sc_fd, buffer, 1024)) > 0) 
-        	write(1, buffer, bytesRead);
-	close(sc_fd);
-    	}}}
-    //}
-*/
-/*else{
-	opcoes de coisas a serem feitas
-	int cs_fd=open("/tmp/fifo_client_server", O_WRONLY);
-	write(cs_fd,buffer,strlen(buffer));
-	close(cs_fd);
-	
-	int bytesRead=0;
-	int sc_fd=open("/tmp/fifo_server_client",O_RDONLY);
-	while((bytesRead=read(sc_fd,buffer,1024))>0)
-		write(0,buffer,bytesRead);
-	close(sc_fd);
-*/	
-    //close(fd);
+	j++;i++;
+    }
+    write(cs_fd,buffer,strlen(buffer));
+    close(cs_fd);
+
+    return 0;
+}
--- grupo-112/src/aurrasd.c
+++ grupo-112/src/aurrasd.c
@@ -14,16 +14,43 @@ typedef struct config* Config;
 int main(int argc, char const *argv[]) {
 	mkfifo("../tmp/fifo_client_server",0644);
 	mkfifo("../tmp/fifo_server_client",0644);
-
-    int fd = open("/tmp/fifo", O_RDONLY);
+	int op=0;
+    
     int bytesRead = 0;
     char buffer[1024];
-    /*while(1) {
-        write(STDOUT_FILENO, buffer, bytesRead);
-    }
-    */
-
-
-    close(fd);
+    while(1) {
+  	char* buffer=malloc(sizeof(char)*1024);
+     	int cs_fd = open("/tmp/fifo_client_server", O_RDONLY);
+	int sc_fd = open("/tmp/fifo_server_client", O_WRONLY);
+	read(cs_fd,buffer,1024);
+	int arg=strlen(buffer);
+	int i=0;
+	char *line= buffer;
+	strsep(&line," ");
+	char** linhas=malloc(sizeof(char*)*30);
+	while(line){
+		linhas[i]=strdup(strsep(&line," ");
+		i++;
+	}
+	if(i<2){
+	char * buf= "./aurras status\n./aurras transform input-filename output-filename filter-id-1 filter-id-2 ...\n";
+	write(sc_fd,buf,strlen(buf));
+	}
+	else{
+	//testa se o programa esta 
+		switch(linhas[1]){
+			case "transform":
+				break;
+			case "status":
+				break;
+			deafault:
+				//erro?
+				break;
+		}
+	}	
+   }
+   close(fd);
+  //  unlink(cs_fd);//rever
+  //  unlink(sc_fd);//rever
     return 0;
 }
